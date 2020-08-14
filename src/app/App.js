4c46f2c65234872147fc1d7eed36e2c37e2d1485
lemon-piepie <purplechilde@163.com>
2020-08-14 14:07:00 +0800

    init

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Route, BrowserRouter} from "react-router-dom";
+import Home from "./Home";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        这是一个新的购物车界面
+      </div>
+    );
+  }
+}
+
+export default App;
